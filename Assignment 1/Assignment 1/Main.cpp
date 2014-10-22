837453b405b3156418db5a0b63cfdb59e533ece0
Jeffrey Smalley <cloudwomega@gmail.com>
2014-10-22 00:21:16 -0400

    Project start
    project made and first changes made

---
+++ Assignment 1/Assignment 1/Main.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+}
