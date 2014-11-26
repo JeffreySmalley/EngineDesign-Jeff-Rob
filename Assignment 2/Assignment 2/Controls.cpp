9c77e2dce80ce32984b174c442a8227befc2f6af
Flembottles <daflemdaga@gmail.com>
2014-11-26 10:05:25 -0500

    Updated Controls

--- Assignment 2/Assignment 2/Controls.cpp
+++ Assignment 2/Assignment 2/Controls.cpp
@@ -1,11 +1,17 @@
 #include "Controls.h"
 
+Gamepads::Gamepads()
+{
+	LX=LY=RX=RY=normalizedLX=normalizedLY=normalizedRX=normalizedRY=RTrigger=LTrigger=0;
+	AButton=BButton=XButton=YButton=RButton=LButton=LSButton=RSButton=StartButton=BackButton=false;
+	normalizedRMagnitude=normalizedLMagnitude=0;
+}
+
 bool Gamepads::isConnected(DWORD index)
 {
 	DWORD dwResult;
-	XINPUT_STATE state;
-	ZeroMemory(&state, sizeof(XINPUT_STATE));
-	dwResult = XInputGetState(index, &state);
+	SecureZeroMemory(&gamepadState, sizeof(XINPUT_STATE));
+	dwResult = XInputGetState(index, &gamepadState);
 	if (dwResult == ERROR_SUCCESS)
 		return true;
 	else
@@ -28,4 +34,47 @@ int Gamepads::getgamepadCount()
 void Gamepads::Update()
 {
 	pads = getgamepadCount();
+	//SecureZeroMemory(&gamepadState, sizeof(XINPUT_STATE));
+	DWORD result;
+	for (int i=0;i<pads;i++)
+	{
+		if(XInputGetState(i,&gamepadState) == ERROR_SUCCESS)
+		{
+			LX = gamepadState.Gamepad.sThumbLX;
+			LY = gamepadState.Gamepad.sThumbLY;
+			
+			LMagnitude = sqrt(LX*LX + LY*LY);
+
+			RX = gamepadState.Gamepad.sThumbRX;
+			RY = gamepadState.Gamepad.sThumbRY;
+
+			RMagnitude = sqrt(RX*RX + RY*RY);
+
+			normalizedLX = LX / LMagnitude;
+
+			if (RMagnitude > XINPUT_DEADZONE)
+			{
+				if(RMagnitude > XINPUT_FLOAT_MAX) RMagnitude = XINPUT_FLOAT_MAX;
+				RMagnitude -= XINPUT_DEADZONE;
+				normalizedRMagnitude = RMagnitude / (XINPUT_FLOAT_MAX - XINPUT_DEADZONE);
+			}
+			else 
+			{
+				RMagnitude = 0;
+				normalizedRMagnitude = 0;
+			}
+			if (LMagnitude > XINPUT_DEADZONE)
+			{
+				if(LMagnitude > XINPUT_FLOAT_MAX) LMagnitude = XINPUT_FLOAT_MAX;
+				LMagnitude -= XINPUT_DEADZONE;
+				normalizedLMagnitude = LMagnitude / (XINPUT_FLOAT_MAX - XINPUT_DEADZONE);
+			}
+			else 
+			{
+				LMagnitude = 0;
+				normalizedLMagnitude = 0;
+			}
+			AButton=((gamepadState.Gamepad.wButtons & XINPUT_GAMEPAD_A)!=0);
+		}
+	}
 }
--- Assignment 2/Assignment 2/Controls.h
+++ Assignment 2/Assignment 2/Controls.h
@@ -1,12 +1,27 @@
 #include <xinput.h>
 #include <dinput.h>
+#include <math.h>
+
+#pragma comment(lib, "Xinput9_1_0.lib")
 
 #define XUSER_MAX_COUNT 1
+#define XINPUT_MAX_TRIGGER_DISTANCE 255
+#define XINPUT_FLOAT_MAX 32767
+#define XINPUT_DEADZONE 7849
+#define XINPUT_TRIGGER_THRESHOLD 15
 
 class Gamepads
 {
 private:
+	float LX,LY,LMagnitude;
+	float RX,RY,RMagnitude;
+	float normalizedLX,normalizedLY;
+	float normalizedRX,normalizedRY;
+	float normalizedRMagnitude,normalizedLMagnitude;
+	bool AButton,BButton,XButton,YButton,RButton,LButton,LSButton,RSButton,StartButton,BackButton;
+	int RTrigger,LTrigger;
 	int pads;
+	XINPUT_STATE gamepadState;
 public:
 	Gamepads();
 	bool isConnected(DWORD index);
